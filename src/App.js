87841322a3a5fd922108a71493ab46798b728a57
Rushikeshpawar4912 <138990079+Rushikeshpawar4912@users.noreply.github.com>
2023-09-10 16:53:16 +0530

    add files

--- src/App.js
+++ src/App.js
@@ -1,35 +1,67 @@
 import './App.css';
-
-import Navbar from './COMPONENTS/Navbar';
-import Footer from './COMPONENTS/Footer';
-import TODO from './COMPONENTS/Tosos.js';
-import Todos from './COMPONENTS/TodoItem';
+import Header from "./MyComponents/Header";
+import { Todos } from "./MyComponents/Todos";
+import { Footer } from "./MyComponents/Footer";
+import { AddTodo } from "./MyComponents/AddTodo";
+import { About } from "./MyComponents/About";
+import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom"; // Updated import
 
 function App() {
-  let todos =[
-    {
-      sno:1,
-      title:"go to the market ",
-      desc:"you need to go to the market"
-    },
-    {
-      sno:1,
-      title:"go to the college ",
-      desc:"you need to go to the college"
-    },
-    {
-      sno:1,
-      title:"go to the school ",
-      desc:"you need to go to the school"
-    },
-   ]
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  }
+  else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  const onDelete = (todo) => {
+    console.log("I am ondelete of todo", todo);
+    setTodos(todos.filter((e) => e !== todo));
+    console.log("deleted", todos)
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
+  const addTodo = (title, desc) => {
+    console.log("I am adding this todo", title, desc)
+    let sno;
+    if (todos.length === 0) {
+      sno = 0;
+    }
+    else {
+      sno = todos[todos.length - 1].sno + 1;
+    }
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+  }
+
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
+
   return (
     <>
-<Navbar title="mytodo" searchbar={true}/>
-<Footer/>
-<TODO/>
-<Todos/>
-</>
+      <Router>
+        <Header title="My Todos List" searchBar={false} />
+        <Routes> {/* Updated to use Routes */}
+          <Route path="/" element={<AddTodo addTodo={addTodo} />} />
+          <Route path="/" element={<Todos todos={todos} onDelete={onDelete} />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
